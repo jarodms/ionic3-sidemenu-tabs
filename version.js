fd462263b7b201f1c2e3feb79bc133dea6215efe
Jarod Smith <jarodms@gmail.com>
2018-06-04 18:10:54 -0500

    minor; fix version message

--- version.js
+++ version.js
@@ -24,4 +24,4 @@ export const VERSION = ${JSON.stringify(gitInfo, null, 4)};
 /* tslint:enable */
 `, { encoding: 'utf-8' });
 
-console.log(`Wrote version info ${gitInfo.raw} to ${relative(resolve(__dirname, '..'), file)}`);
+console.log(`Wrote version ${gitInfo.version} info to ${relative(resolve(__dirname, '..'), file)}`);
